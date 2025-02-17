b53f89e00ebde44a80fe0ccb941dfe6204c1d0e1
GangOn0215 <ekdldktnekf@gmail.com>
2025-02-17 11:36:00 +0900

    [ INIT ]

---
+++ src/main/java/amo/interior/InteriorApplication.java
@@ -0,0 +1,13 @@
+package amo.interior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteriorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InteriorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/amo/interior/InteriorApplicationTests.java
@@ -0,0 +1,13 @@
+package amo.interior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteriorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
