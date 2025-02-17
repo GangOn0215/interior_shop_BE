3c08b19f70efca660487cb525457219caa5d8f80
GangOn0215 <ekdldktnekf@gmail.com>
2025-02-17 13:37:31 +0900

    [ INIT POM ]

--- src/main/java/amo/interior/InteriorApplication.java
+++ src/main/java/amo/interior/InteriorApplication.java
@@ -1,9 +1,10 @@
 package amo.interior;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@EnableAutoConfiguration
 public class InteriorApplication {
 
     public static void main(String[] args) {
